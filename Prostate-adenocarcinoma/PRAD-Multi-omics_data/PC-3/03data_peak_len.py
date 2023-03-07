cfc8f5187b9ee413d10c6d050a65f55208a0a803
刘纯 <1742047239@qq.com>
2023-03-08 03:17:27 +0800

    PRAD

---
+++ Prostate-adenocarcinoma/PRAD-Multi-omics_data/PC-3/03data_peak_len.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+from Bio import SeqIO
+from pandas import DataFrame
+
+# chr1	752559	752863
+
+omics_data = ['H3K4me1-ENCFF312LEI', 'H3K4me3-ENCFF010DLV', 'H3K9me3-ENCFF781KAE', 'H3K27ac-ENCFF860AQW', 'H3K36me3-ENCFF985PEP']
+for i in omics_data:
+    print("这是组学数据：", i)
+    content_liver_omics = []
+    with open("/data/lyli/Prostate-adenocarcinoma/PRAD-Multi-omics_data/PC-3/" + i + ".bed")as f:
+        for line in f:
+            content_liver_omics.append(line.strip().split())    # strip()表示删除掉数据中的换行符，split（‘ ’）则是数据中遇到空格就隔开
+
+    n = 0
+    sum = 0
+    omics_len = []
+    for j in content_liver_omics:
+        chrom = j[0].strip()
+        chrom = str(chrom)
+        chromStart = int(j[1])
+        chromEnd = int(j[2])
+        len = chromEnd - chromStart
+        omics_len.append(len)
+        sum = sum + len
+        n = n + 1
+    print("，有"+str(n)+"个peak。\n"+"peak的总长度为："+str(sum) +"\n"+ "peak的平均长度为："+str(sum/n))
+    newfile = open("/data/lyli/Prostate-adenocarcinoma/PRAD-Multi-omics_data/PC-3/omics_data_len/" + i + ".txt", 'w')
+    for a in omics_len:
+        newfile.write(str(a) + "\n")
+    newfile.close()
