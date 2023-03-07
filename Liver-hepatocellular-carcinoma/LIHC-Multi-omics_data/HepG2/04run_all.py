f43f257e1544cf0be2cfd88eda9cb894590d4a00
刘纯 <1742047239@qq.com>
2023-03-08 00:30:39 +0800

    LIHC

--- Liver-hepatocellular-carcinoma/LIHC-Multi-omics_data/HepG2/04run_all.py
+++
@@ -1,4 +0,0 @@
-import os
-windows = ['04enhancer_peak_distribution01','04enhancer_peak_distribution02', '04enhancer_peak_distribution03', '04enhancer_peak_distribution04', '04enhancer_peak_distribution05']
-for i in windows:
-    os.system("nohup python -u "+i+".py >> "+i+".log 2>&1 &")
